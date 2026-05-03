37faa1c2324e4b9b8b2f22a855a55bbc53b41c10
Juan Emilio Vasquez Encalada <202312207@urp.edu.pe>
2026-05-02 20:22:07 -0500

    Remove gendered avatars and unused import
    correro avatar

--- admin/src/components/Header.jsx
+++ admin/src/components/Header.jsx
@@ -1,26 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "../styles/Header.css";
-
-const MaleSVG = () => (
-  <svg viewBox="0 0 64 64" width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="32" cy="32" r="32" fill="#1e90ff" />
-    <g transform="translate(12,10)" fill="#fff">
-      <circle cx="20" cy="12" r="8" />
-      <path d="M2 44c0-11 18-11 18-11s18 0 18 11v3H2v-3z" />
-    </g>
-  </svg>
-);
-
-const FemaleSVG = () => (
-  <svg viewBox="0 0 64 64" width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="32" cy="32" r="32" fill="#d63384" />
-    <g transform="translate(12,10)" fill="#fff">
-      <circle cx="20" cy="12" r="8" />
-      <path d="M14 38c0-6-6-10-10-10s-4 4-4 10h14z" />
-    </g>
-  </svg>
-);
 
 const Header = () => {
   const [session, setSession] = useState(null);
@@ -38,12 +17,10 @@ const Header = () => {
 
   if (!session || !session.user) return null;
 
-  const { email, picture, gender } = session.user;
+  const { email, picture } = session.user;
 
   const Avatar = () => {
     if (picture) return <img src={picture} alt="avatar" className="header-avatar-img" />;
-    if (gender && gender.toLowerCase && gender.toLowerCase().startsWith("m")) return <MaleSVG />;
-    if (gender && gender.toLowerCase && gender.toLowerCase().startsWith("f")) return <FemaleSVG />;
     return (
       <svg viewBox="0 0 64 64" width="40" height="40" xmlns="http://www.w3.org/2000/svg">
         <circle cx="32" cy="32" r="32" fill="#6b7280" />
